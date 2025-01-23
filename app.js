e84aa52a6514a0c4da57e2c7e460f71b7d32a730
Ayush sen <senayush920@gmail.com>
2025-01-24 00:12:57 +0530

    add

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'https://myEvents.onrender.com/auth/google/callback',
+      callbackURL: 'https://myevents-svbt.onrender.com/auth/google/callback',
       scope: [
         "profile",
         "email",
